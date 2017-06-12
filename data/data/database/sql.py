89b626b2eac5d638db8350908f7b3f83b2432e58
liuliang <1432249204@qq.com>
2017-06-12 17:41:30 +0800

    create_tables

--- data/data/database/sql.py
+++ data/data/database/sql.py
@@ -5,15 +5,76 @@ db = MySQLdb.connect("localhost","root","huochai123","gsa" )
 cursor = db.cursor()
 
 def create_tables():
-  sqls = []
-  sqls[0] = """
-            create table if not exists user(
-            login  ,
-            `runoob_id` INT UNSIGNED AUTO_INCREMENT,
-   `runoob_title` VARCHAR(100) NOT NULL,
-   `runoob_author` VARCHAR(40) NOT NULL,
-   `submission_date` DATE,
-   PRIMARY KEY ( `runoob_id` )
-            )ENGINE=InnoDB DEFAULT CHARSET=utf8; """
+  sqls =  [
+          """
+            CREATE TABLE IF NOT EXISTS user(
+            login  VARCHAR(128) NOT NULL,
+            id INT UNSIGNED,
+            url VARCHAR(256),
+            html_url VARCHAR(256),
+            followers_url VARCHAR(256),
+            following_url VARCHAR(256),
+            starred_url VARCHAR(256),
+            repos_url VARCHAR(256),
+            type CHAR(64),
+            site_admin CHAR(10),
+            name VARCHAR(128),
+            company VARCHAR(128),
+            location VARCHAR(128),
+            hireable CHAR(10),
+            public_repos INT UNSIGNED,
+            public_gists INT UNSIGNED,
+            followers INT UNSIGNED,
+            following INT UNSIGNED,
+            created_at CHAR(64),
+            updated_at CHAR(64),
+            spider_updated_at CHAR(64),
+            PRIMARY KEY (login)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+          """,
+          """
+            CREATE TABLE IF NOT EXISTS repo(
+            full_name  VARCHAR(128) NOT NULL,
+            id INT UNSIGNED,
+            owner_login VARCHAR(128),
+            name VARCHAR(128),
+            private CHAR(10),
+            fork CHAR(10),
+            url VARCHAR(256),
+            html_url VARCHAR(256),
+            stargazers_url VARCHAR(256),
+            language VARCHAR(64),
+            forks_count INT UNSIGNED,
+            stargazers_count INT UNSIGNED,
+            watchers_count INT UNSIGNED,
+            size INT UNSIGNED,
+            open_issues_count INT UNSIGNED,
+            push_at CHAR(64),
+            created_at CHAR(64),
+            updated_at CHAR(64),
+            spider_updated_at CHAR(64),
+            PRIMARY KEY (full_name)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+          """,
+          """
+            CREATE TABLE IF NOT EXISTS user_user(
+            followed  VARCHAR(128) NOT NULL,
+            following VARCHAR(128) NOT NULL,
+            PRIMARY KEY (followed,following)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          """,
+          """
+            CREATE TABLE IF NOT EXISTS user_repo(
+            user VARCHAR(128) NOT NULL,
+            repo VARCHAR(128) NOT NULL,
+            PRIMARY KEY (user,repo)
+            )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+          """
+         ]
+  for s in sqls:
+    print "EXECUTE CREATE TABLE : \n"+s
+    cursor.execute(s)
 
-  cursor.execute(sql)
+def insert(table_name,item):
+  s = """
+	INSERT
